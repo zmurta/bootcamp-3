6807fcf3b967a0f674d98d542bfc3ccf3ea4562a
Zach <zmurta@ufl.edu>
2017-09-05 23:38:10 -0400

    changed JSONtoMongo.js

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,14 +10,41 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
+mongoose.connect(config.db.uri);
+
+var listingData = fs.readFileSync('listings.json', 'utf8');
+listingData = JSON.parse(listingData);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
+for(var i = 0; i < listingData.entries.length; i++) {
+  if(listingData.entries[i].coordinates !== undefined) {
+    var temp = new Listing({
+      code : listingData.entries[i].code,
+      name : listingData.entries[i].name,
+      coordinates : {
+        latitude : listingData.entries[i].coordinates.latitude,
+        longitude : listingData.entries[i].coordinates.longitude
+      },
+      address : listingData.entries[i].address
+    });
+  }
+  else {
+    var temp = new Listing({
+      code : listingData.entries[i].code,
+      name : listingData.entries[i].name
+    });
+  }
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
  */
+
+  temp.save( (err) => {
+    if(err) throw err;
+    console.log("Saved!");
+  });
+}
