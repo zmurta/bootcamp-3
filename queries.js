b19c10a8d4e7d3f9584d254cc8c13da7cdbac67f
Zach <zmurta@ufl.edu>
2017-09-07 00:36:54 -0400

    Submission ready

--- queries.js
+++ queries.js
@@ -1,13 +1,9 @@
-var mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
-
-/* Connect to your database */
-mongoose.connect(config.db.uri);
-
 /* Fill out these functions using Mongoose queries*/
 var findLibraryWest = function() {
+  /* 
+    Find the document that contains data corresponding to Library West,
+    then log it to the console. 
+   */
   Listing.find( { name : 'Library West' }, (err, listing) => {
     if(err) throw err;
 
@@ -16,6 +12,11 @@ var findLibraryWest = function() {
 };
 
 var removeCable = function() {
+  /*
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
+    and remove this listing from your database and log the document to the console. 
+   */
   Listing.findOneAndRemove( { code : 'CABL' }, (err, listing) => {
     if(err) throw err;
     console.log(listing);
@@ -23,6 +24,10 @@ var removeCable = function() {
 };
 
 var updatePhelpsLab = function() {
+  /*
+    Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
+    log the updated document to the console. 
+   */
   Listing.findOneAndUpdate( 
     { name : 'Phelps Laboratory' }, 
     { address : 'Phelps Lab, Gainesville, FL 32603, United States' } , 
@@ -33,6 +38,9 @@ var updatePhelpsLab = function() {
 };
 
 var retrieveAllListings = function() {
+  /* 
+    Retrieve all listings in the database, and log them to the console. 
+   */
   Listing.find({}, function(err, listings) {
     if (err) throw err;
 
